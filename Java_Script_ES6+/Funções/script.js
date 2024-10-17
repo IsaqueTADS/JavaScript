6bd62b0e3ea20a5fa8f7d445f1f265129f39a3e6
IsaqueTADS Ubuntu <isaqueofc18@gmail.com>
2024-10-17 17:29:12 -0300

    funcao

--- Java_Script_ES6+/Funções/script.js
+++ Java_Script_ES6+/Funções/script.js
@@ -7,8 +7,27 @@ function faleResultado(res1) {
   console.log(`oi o resultado passado por callBack é ${res1}`);
 }
 
-areaQuadrado(5, faleResultado);
+console.log(areaQuadrado(5, faleResultado));
+// addEventListener("click", function () {
+//   console.log("JESUS ME GUIA ATRAVAES DE UM MUDO OBSCURO");
+// });
 
-addEventListener("click", function () {
-  console.log("JESUS ME GUIA ATRAVAES DE UM MUDO OBSCURO");
-});
+var res2 = faleResultado();
+
+console.log(`o retorno sem retunr será ${res2}`);
+
+function verifiIdade(idade) {
+  console.log(idade);
+  if (typeof idade !== "number") {
+    return "informe a idade corretamente! ";
+  } else if (idade >= 60) {
+    console.log("é vey sor, idosão");
+    return true;
+  } else {
+    console.log("é novao ainda");
+    return false;
+  }
+}
+
+let res3 = verifiIdade(500);
+console.log(res3);
