d5c88a2bbaee05aa76485f593ea7b25b771908d5
IsaqueTADS Ubuntu <isaqueofc18@gmail.com>
2024-10-16 22:26:01 -0300

    inicio

---
+++ Java_Script_ES6+/DOM-1/script.js
@@ -0,0 +1,10 @@
+
+
+const varaSeca = window.document.location.href;
+
+console.log (varaSeca);
+
+
+if ( varaSeca === 'http://127.0.0.1:5500/DOM-1/index.html'){
+  console.log('Eita preulaa')
+}
---
+++ Java_Script_ES6+/Objeto-1/script.js
@@ -0,0 +1,28 @@
+var user = {
+  name: "Isaque",
+  surName: "Rodrigues",
+  fullName() {
+    return `${this.name} ${this.surName}`;
+  },
+};
+
+var carro = {
+  preco: 1000,
+  portas: 4,
+  marca: "Audi R8",
+};
+
+carro.preco = 784000;
+
+var cachorro = {
+  raca: "labarador",
+  cor: "Preto",
+  idade: 10,
+  latir(pessoa) {
+    if (pessoa === "homem") {
+      return "latir";
+    } else {
+      return "gadooo";
+    }
+  },
+};
