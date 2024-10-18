485eeede688c8dd2c4af0ebf21059a231586523b
IsaqueTADS Ubuntu <isaqueofc18@gmail.com>
2024-10-18 05:06:31 -0300

    ex

--- Java_Script_ES6+/DOM-1/script2.js
+++ Java_Script_ES6+/DOM-1/script2.js
@@ -1,1 +1,9 @@
-  
+const local = window.location.href;
+console.log(local);
+
+const elementoCaraDePauAtivoKK = document.querySelectorAll(".ativo");
+
+console.log(innerWidth, innerHeight);
+
+
+console.log(navigator.language)
