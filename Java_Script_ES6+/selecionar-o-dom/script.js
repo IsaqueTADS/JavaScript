de150ee74f32ca13c5e21b8397ea5fb9550ce86f
IsaqueTADS Ubuntu <isaqueofc18@gmail.com>
2024-10-24 01:26:04 -0300

    aprendemso soubre nodelist e htmllist sem contar alguns metodos e tambem converter list para array

--- Java_Script_ES6+/selecionar-o-dom/script.js
+++ Java_Script_ES6+/selecionar-o-dom/script.js
@@ -19,3 +19,31 @@ console.log(linksInternos.href);
 const listaImgs = document.querySelectorAll(".animais-lista img");
 
 console.log(listaImgs[0]);
+
+var primeiraul = document.querySelector("ul");
+console.log(primeiraul.innerHTML);
+
+const nodeList = document.querySelectorAll(".grid-section");
+const htmlList = document.getElementsByClassName("grid-section");
+
+primeiraul.classList.add("grid-section");
+
+console.log(nodeList);
+console.log(htmlList);
+
+/* Array like : HTMLCollection e NodeList sõa array-like, parecem uma array mas não são. O método de Array forEach() por exemplo, existe apenas em NodeList */
+
+nodeList.forEach((gridItem, index, array) => {
+  gridItem.classList.add("azul");
+  console.log(index);
+  console.log(gridItem);
+});
+
+//tranformando arrayy like em array
+const arrayGrid = Array.from(htmlList);
+
+console.log(arrayGrid);
+
+arrayGrid.forEach((item) => {
+  console.log(item);
+});
